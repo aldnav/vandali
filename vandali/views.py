543ce93d30cc8a2820d686736daec1753407f1f3
aldnav <aldrinnavarro16@gmail.com>
2014-07-23 23:49:31 +0800

    Added functions on other routes

--- vandali/views.py
+++ vandali/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render, render_to_response, redirect
 
 def index(request):
 	return render(request, 'vandali/index.html', {})
+
+def about(request):
+	pass
+
+def portfolio(request):
+	pass
+
+def contact(request):
+	pass
+
+def blog(request):
+	pass
