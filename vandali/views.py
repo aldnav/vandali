bf4853b2b7b7519c871b12d93a30be48198f9e95
aldnav <aldrinnavarro16@gmail.com>
2014-07-23 23:37:08 +0800

    Added index on view

--- vandali/views.py
+++ vandali/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response, redirect
 
-# Create your views here.
+def index(request):
+	return render(request, 'vandali/index.html', {})
