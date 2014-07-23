c9091b97f6257d1ddd1d5f2e5cc5e14b5fa4a7fb
aldnav <aldrinnavarro16@gmail.com>
2014-07-24 02:08:22 +0800

    Reroute about, portfolio, contact and blog to respective templates

--- vandali/views.py
+++ vandali/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render, render_to_response, redirect
 
 def index(request):
-	return render(request, 'vandali/index.html', {})
+    return render(request, 'vandali/index.html')
 
 def about(request):
-	pass
+    return render(request, 'vandali/about.html')
 
 def portfolio(request):
-	pass
+    return render(request, 'vandali/portfolio.html')
 
 def contact(request):
-	pass
+    return render(request, 'vandali/contact.html')
 
 def blog(request):
-	pass
+    return render(request, 'vandali/blog.html')
