e2c8c5c0279188789c6228f9df5c7d7711cd63fa
aldnav <aldrinnavarro16@gmail.com>
2014-07-23 23:48:39 +0800

    Fixed bug on patterns

--- vandali/urls.py
+++ vandali/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 
 from vandali import views
 
-urlpatterns = patterns(
+urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
     url(r'^portfolio/$', views.portfolio, name='portfolio'),
